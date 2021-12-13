27dcb416da67f9d1a4289b796d94bc136daddb0c
joelnavarrodesoft <joelnavarrodesoft@gmail.com>
2021-12-13 10:25:12 -0500

    Added nameQuestion to SectionForm

--- schemas/SectionFormSchema.js
+++ schemas/SectionFormSchema.js
@@ -3,14 +3,16 @@ const Joi = require('joi');
 const SectionFormCreate = Joi.object().keys({
     description: Joi.string().required(),
     form_available: Joi.string().required(),
-    duplicate: Joi.boolean()
+    duplicate: Joi.boolean(),
+    nameQuestion: Joi.string()
 });
 
 const SectionFormUpdate = Joi.object().keys({
     _id: Joi.string().required(),
     description: Joi.string().required(),
     form_available: Joi.string().required(),
-    duplicate: Joi.boolean()
+    duplicate: Joi.boolean(),
+    nameQuestion: Joi.string()
 });
 
 module.exports = {
