5460cd7c056a4575e91aa3fefe490d233f6adcf6
joelnavarrodesoft <joelnavarrodesoft@gmail.com>
2021-12-28 12:22:00 -0500

    Added create_by update schemma

--- schemas/CostsAndIncomeSchema.js
+++ schemas/CostsAndIncomeSchema.js
@@ -19,6 +19,7 @@ const CostsAndIncomeCreate = Joi.object().keys({
 
 const CostsAndIncomeUpdate = Joi.object().keys({
     _id: Joi.string().required(),
+    create_by: Joi.string().required(),
     registry_number: Joi.string().required(),
     business_name_establishment: Joi.string().required(),
     person_type: Joi.string().required(),
