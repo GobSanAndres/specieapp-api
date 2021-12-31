6262eba77aa6fbad7315ac5ac9f149c8f5c4028d
joelnavarrodesoft <joelnavarrodesoft@gmail.com>
2021-12-30 23:18:50 -0500

    Update industrialvessel schemma

--- schemas/IndustrialVesselInspectionSchema.js
+++ schemas/IndustrialVesselInspectionSchema.js
@@ -46,6 +46,7 @@ const IndustrialVesselInspectionCreate = Joi.object().keys({
 
 const IndustrialVesselInspectionUpdate = Joi.object().keys({
     _id: Joi.string().required(),
+    create_by: Joi.string().required(),
     register_number: Joi.string().required(),
     type_fishshop: Joi.string().required(),
     landing_site: Joi.string().required(),
