c0df86275b6e5605103e38103f4279977ee01de0
oliver-rdgz <olivert199@hotmail.com>
2022-03-11 09:13:32 -0500

    endPoint para los de san Andres para obtener data

--- routes/GenericRoutes.js
+++ routes/GenericRoutes.js
@@ -15,6 +15,8 @@ router.post("/app/create", [validTokenApp], saveData);
 
 router.get('/app/getdata', [validTokenApp], getSearch);
 
+router.get('/app/getdataAll', getSearch);
+
 router.get('/getdata', [validJWT], getSearch);
 
 router.get('/app/gethistoric', [validTokenApp, validator(GenericHistoric)], historicByUser);
