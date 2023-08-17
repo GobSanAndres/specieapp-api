28f345e7d3e3b523fe44970166f42a97d5781011
oliver-rdgz <olivert199@hotmail.com>
2023-08-17 18:11:46 -0500

    instalando morgan para ver logs

--- server.js
+++ server.js
@@ -1,55 +1,53 @@
-const express = require('express');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const { routes } = require('./constants/routes');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const fileUpload = require("express-fileupload");
+const { routes } = require("./constants/routes");
 
-const { dbConnection } = require('./database/config');
-
+const { dbConnection } = require("./database/config");
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-    constructor(){
-        this.app = express();
-        this.port = process.env.PORT;
+    //Conectar a base de datos
+    this.connectionDB();
 
-        //Conectar a base de datos
-        this.connectionDB();
+    //Middlewares
+    this.middlewares();
 
-        //Middlewares
-        this.middlewares();
+    this.routes();
+  }
 
-        this.routes();
+  async connectionDB() {
+    await dbConnection();
+  }
 
+  middlewares() {
+    this.app.use(morgan("combined"));
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+        createParentPath: true,
+      })
+    );
+  }
 
-        
-    }
+  routes() {
+    routes.forEach((item) => {
+      this.app.use(item.path, item.route);
+    });
+  }
 
-    async connectionDB(){
-        await dbConnection();
-    }
-
-    middlewares(){
-        this.app.use(cors());
-        this.app.use(express.json());
-        this.app.use(fileUpload({
-            useTempFiles: true,
-            tempFileDir: "/tmp/",
-            createParentPath: true
-        }));
-    }
-
-    routes(){
-        routes.forEach(item => {
-            this.app.use(item.path, item.route)
-        })
-    }
-
-    listen(){
-        this.app.listen(this.port, () => {
-            console.log("Servidor corriendo en puerto ", this.port);
-        });
-    }
-
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto ", this.port);
+    });
+  }
 }
 
 module.exports = Server;
