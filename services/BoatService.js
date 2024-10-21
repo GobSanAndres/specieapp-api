dfd1c42aa8ccacfe4a8c30fb15592fd2dde416b9
Jose Ruiz Urango <joseurango777@gmail.com>
2024-10-21 14:51:29 -0500

    update rgp services

--- services/BoatService.js
+++ services/BoatService.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const listBoats = async(req) => {
     try{
         let auth = await getToken();
-        let boats = await axios.get(`${process.env.API_RPG}api/rpg-sai/external-api/v1/all-boats/${req.params.type}`, {...auth, params: req.query});
+        let boats = await axios.get(`${process.env.API_RPG}api/rpg-sai/external-api/v1/apprgp/all-boats/${req.params.type}`, {...auth, params: req.query});
         return boats.data;
 
     }catch(error){
